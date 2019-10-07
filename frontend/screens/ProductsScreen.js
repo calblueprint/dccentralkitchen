9c4e7c9e4e7619f858cd15c61303565c47ac0c37
Tommy Poa <tommypoa@berkeley.edu>
2019-10-07 14:03:01 -0700

    ScrollView works, but trying FlatList.

--- frontend/screens/ProductsScreen.js
+++ frontend/screens/ProductsScreen.js
@@ -12,6 +12,9 @@ import {
   Text,
   TouchableOpacity,
   View,
+  FlatList,
+  Dimensions,
+
 } from 'react-native';
 
 import { MonoText } from '../components/StyledText';
@@ -23,12 +26,18 @@ const base = new Airtable({ apiKey: ''}).base(
 );
 const productsTable = base("Products").select({view: "Grid view"})
 var productsList = [];
+var keyValue;
 productsTable.firstPage((err, records) => {
     if (err) {
         console.error(err);
         return;
     }
-    productsList = records.map(record => record.get("Name"))
+    productsList = records.map(record => {record.get("Name")})
+    idList = records.map(record => {record.id})
+    keyValue =  idList.reduce(function(result, field, index) {
+        result[productsList[index]] = field;
+        return result;
+      }, {})
     // console.log(productsList)
 })
 
@@ -36,24 +45,43 @@ class ProductsScreen extends React.Component {
     constructor(props) {
       super(props);
       this.state = {
-          productsList: productsList
+          productsList: productsList,
+          keyValue: keyValue
       };
     }
     
 
     render() {
-        const { productsList } = this.state
+        const { productsList, keyValue } = this.state
         console.log(productsList)
         return (
-            <View>
-                {productsList.map((product) => {
-                    return (
-                    <Text>{product}</Text>
-                    )})
-                }
-            </View>
-            )
+            <FlatList 
+                style = {styles.container}
+                numColumns = {2}
+                data = {keyValue}
+                renderItem={({ product }) => (
+                    <View style={styles.item}>
+                        <Text>{product.key}</Text>
+                    </View>
+                    
+                )}>
+            </FlatList>
+        )
     }
 }
 
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      marginVertical: 20,
+    },
+    item: {
+    //   backgroundColor: '#4D243D',
+      alignItems: 'center',
+      justifyContent: 'center',
+      flex: 1,
+      margin: 1,
+      height: Dimensions.get('window').width / 2
+    }
+})
 export default ProductsScreen;
