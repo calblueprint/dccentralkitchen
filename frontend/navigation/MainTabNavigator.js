a2a07d44bf17c76d05070f57eb780805b6c47aff
Tommy Poa <tommypoa@berkeley.edu>
2019-10-07 01:29:23 -0700

    Pulled products from airtable. Products page in progress.

--- frontend/navigation/MainTabNavigator.js
+++ frontend/navigation/MainTabNavigator.js
@@ -6,6 +6,7 @@ import TabBarIcon from '../components/TabBarIcon';
 import HomeScreen from '../screens/HomeScreen';
 import LinksScreen from '../screens/LinksScreen';
 import SettingsScreen from '../screens/SettingsScreen';
+import ProductsScreen from '../screens/ProductsScreen';
 
 const config = Platform.select({
   web: { headerMode: 'screen' },
