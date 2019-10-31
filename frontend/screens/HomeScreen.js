34a6dea05924879f7447c2e9d6e53236c5932947
Annie Ro <anniero@berkeley.edu>
2019-10-30 19:16:41 -0700

    Cleanup home screen etc

--- frontend/screens/HomeScreen.js
+++ frontend/screens/HomeScreen.js
@@ -1,4 +1,3 @@
-import * as WebBrowser from 'expo-web-browser';
 import React from 'react';
 import {
   Image,
@@ -6,16 +5,12 @@ import {
   ScrollView,
   StyleSheet,
   Text,
-  TouchableOpacity,
   View,
   AsyncStorage,
   Button
 } from 'react-native';
 
-import { MonoText } from '../components/StyledText';
-
 export default class HomeScreen extends React.Component {
-  
   // Sign out function -- it clears the local storage then navigates
   // to the authentication page.
   _signOutAsync = async () => {
@@ -28,7 +23,8 @@ export default class HomeScreen extends React.Component {
       <View style={styles.container}>
         <ScrollView
           style={styles.container}
-          contentContainerStyle={styles.contentContainer}>
+          contentContainerStyle={styles.contentContainer}
+        >
           <View style={styles.welcomeContainer}>
             <Image
               source={
@@ -43,62 +39,39 @@ export default class HomeScreen extends React.Component {
           <View style={styles.getStartedContainer}>
             <DevelopmentModeNotice />
 
-            <Text style={styles.getStartedText}>Get started by opening</Text>
+            <Text style={styles.helloTitle}>Hello DC!</Text>
 
-            <View
-              style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-              <MonoText>screens/HomeScreen.js</MonoText>
-            </View>
+            <Text style={styles.helloMessage}>
+              Remember to update your tasks in Asana and be taking notes in
+              Documentation Directory as you go (":
+            </Text>
 
-            <Text style={styles.getStartedText}>
-              Change this text and your app will automatically reload.
+            <Text style={styles.helloMessage}>
+              Ping in Slack if you get stuck and we shall try to un-stuck you
+              #CherishEachOther
             </Text>
           </View>
 
-          <View style={styles.helpContainer}>
-            <Button 
+          <View style={styles.signOutContainer}>
+            <Button
               title="Sign out"
-              onPress={this._signOutAsync} 
-              style={styles.helpLink}>
-            </Button>
+              onPress={this._signOutAsync}
+              style={styles.signOutButton}
+            ></Button>
           </View>
         </ScrollView>
-
-        <View style={styles.tabBarInfoContainer}>
-          <Text style={styles.tabBarInfoText}>
-            This is a tab bar. You can edit it in:
-          </Text>
-
-          <View
-            style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-            <MonoText style={styles.codeHighlightText}>
-              navigation/MainTabNavigator.js
-            </MonoText>
-          </View>
-        </View>
       </View>
     );
   }
 }
 
 HomeScreen.navigationOptions = {
-  header: null,
+  header: null
 };
 
 function DevelopmentModeNotice() {
   if (__DEV__) {
-    const learnMoreButton = (
-      <Text onPress={handleLearnMorePress} style={styles.helpLinkText}>
-        Learn more
-      </Text>
-    );
-
-    return (
-      <Text style={styles.developmentModeText}>
-        Development mode is enabled: your app will be slower but you can use
-        useful development tools. {learnMoreButton}
-      </Text>
-    );
+    return <Text style={styles.developmentModeText}>~development mode~</Text>;
   } else {
     return (
       <Text style={styles.developmentModeText}>
@@ -108,103 +81,61 @@ function DevelopmentModeNotice() {
   }
 }
 
-function handleLearnMorePress() {
-  WebBrowser.openBrowserAsync(
-    'https://docs.expo.io/versions/latest/workflow/development-mode/'
-  );
-}
-
-function handleHelpPress() {
-  WebBrowser.openBrowserAsync(
-    'https://docs.expo.io/versions/latest/workflow/up-and-running/#cant-see-your-changes'
-  );
-}
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#fff'
   },
   developmentModeText: {
     marginBottom: 20,
     color: 'rgba(0,0,0,0.4)',
     fontSize: 14,
     lineHeight: 19,
-    textAlign: 'center',
+    textAlign: 'center'
   },
   contentContainer: {
-    paddingTop: 30,
+    paddingTop: 30
   },
   welcomeContainer: {
     alignItems: 'center',
     marginTop: 10,
-    marginBottom: 20,
+    marginBottom: 20
   },
   welcomeImage: {
     width: 100,
     height: 80,
     resizeMode: 'contain',
     marginTop: 3,
-    marginLeft: -10,
+    marginLeft: -10
   },
   getStartedContainer: {
     alignItems: 'center',
-    marginHorizontal: 50,
+    marginHorizontal: 50
   },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
+  helloTitle: {
     fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
+    fontWeight: 'bold',
+    color: 'rgba(13, 99, 139, 0.8)',
     lineHeight: 24,
-    textAlign: 'center',
+    textAlign: 'center'
   },
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { width: 0, height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
+  helloMessage: {
+    paddingVertical: 10,
+    fontSize: 15.5,
+    fontWeight: '600',
+    color: 'rgba(30, 183, 255, 0.8)',
+    lineHeight: 20,
+    textAlign: 'left'
   },
   navigationFilename: {
-    marginTop: 5,
+    marginTop: 5
   },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
+  signOutContainer: {
+    marginTop: 20,
+    alignItems: 'center'
   },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
-  },
+  signOutButton: {
+    fontSize: 20,
+    paddingVertical: 15
+  }
 });
