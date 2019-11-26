26c28a28638c27ed0cde093d0089818c71e64ba5
Annie Ro <anniero@berkeley.edu>
2019-11-25 23:26:32 -0800

    Fix BASE usage

--- screens/Camera.js
+++ screens/Camera.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Text, View, TouchableOpacity, Image } from 'react-native';
+import { Text, View, TouchableOpacity } from 'react-native';
 import * as Permissions from 'expo-permissions';
 import { Camera } from 'expo-camera';
-import { BASE, IMG_KEY } from '../lib/common';
+import BASE, { IMG_KEY } from '../lib/common';
 
 export default class ReceiptScanner extends React.Component {
   constructor(props) {
@@ -14,7 +14,7 @@ export default class ReceiptScanner extends React.Component {
       uploadStatus: ''
     };
   }
-  
+
   async componentDidMount() {
     const { status } = await Permissions.askAsync(Permissions.CAMERA);
     this.setState({ hasCameraPermission: status === 'granted' });
@@ -25,25 +25,25 @@ export default class ReceiptScanner extends React.Component {
   // @TODO: Johnathan add functionality for user to confirm the image upload.
   snap = async () => {
     if (this.camera) {
-      this.camera.takePictureAsync({base64: true, quality: 0}).then(photo => {
-        this.camera.pausePreview()
-        let base64 = `data:image/jpeg;base64,${photo["base64"]}`
+      this.camera.takePictureAsync({ base64: true, quality: 0 }).then(photo => {
+        this.camera.pausePreview();
+        const base64 = `data:image/jpeg;base64,${photo.base64}`;
         this.setState({
           uploadStatus: 'Uploading...'
-        })
-        this._uploadImage(base64)
-      })
+        });
+        this._uploadImage(base64);
+      });
     }
   };
 
   // Uploads the base64 binary image to cloudinary and then uses that generated URL
   // to create an Airtable record with the image.
-  _uploadImage = async (imageBinary) => {
-    let apiUrl = 'https://api.cloudinary.com/v1_1/dxecwnkxx/image/upload';
-    let data = {
-      "file": imageBinary,
-      "upload_preset": IMG_KEY,
-    }
+  _uploadImage = async imageBinary => {
+    const apiUrl = 'https://api.cloudinary.com/v1_1/dxecwnkxx/image/upload';
+    const data = {
+      file: imageBinary,
+      upload_preset: IMG_KEY
+    };
     fetch(apiUrl, {
       method: 'POST',
       headers: {
@@ -51,88 +51,99 @@ export default class ReceiptScanner extends React.Component {
       },
       body: JSON.stringify(data)
     })
-    .then(data => {
-      return data.json()
-    })
-    .then(data => {
-      let postUrl = data.secure_url
-      return BASE('images').create([
-        {
-          "fields": {
-            "Attachments": [
-              {
-                "url": postUrl
-              }
-            ]
-          }
-        }
-      ], {typecast: true})
-    })
-    .then(() => {
-      this.setState({
-        uploadStatus: "Upload Completed"
+      .then(resp => {
+        return resp.json();
       })
-    })
-  }
+      .then(resp => {
+        const postUrl = resp.secure_url;
+        return BASE('images').create(
+          [
+            {
+              fields: {
+                Attachments: [
+                  {
+                    url: postUrl
+                  }
+                ]
+              }
+            }
+          ],
+          { typecast: true }
+        );
+      })
+      .then(() => {
+        this.setState({
+          uploadStatus: 'Upload Completed'
+        });
+      });
+  };
 
   render() {
     const { hasCameraPermission } = this.state;
+    // Before asking for permissions, renders blank screen
     if (hasCameraPermission === null) {
       return <View />;
-    } else if (hasCameraPermission === false) {
-      return <Text>No access to camera</Text>;
-    } else {
-      return (
-        <View style={{ flex: 1 }}>
-          <Camera 
-            style={{ flex: 1 }} 
-            type={this.state.type}
-            ref={ref => {
-              this.camera = ref;
-            }}
-            base64={true}
-            >
-            <View
-              style={{
-                flex: 1,
-                backgroundColor: 'transparent',
-                flexDirection: 'row',
-              }}>
-              <TouchableOpacity
-                style={{
-                  flex: 0.1,
-                  alignSelf: 'flex-end',
-                  alignItems: 'center',
-                }}
-                onPress={() => {
-                  this.setState({
-                    type:
-                      this.state.type === Camera.Constants.Type.back
-                        ? Camera.Constants.Type.front
-                        : Camera.Constants.Type.back,
-                  });
-                }}>
-                <Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}> Flip </Text>
-              </TouchableOpacity>
-              <TouchableOpacity
-                style={{
-                  flex: 0.8,
-                  alignSelf: 'flex-end',
-                  alignItems: 'center',
-                }}
-                onPress={
-                  () => this.snap()
-                  }>
-                <Text style={{ fontSize: 40, marginBottom: 10, color: 'white' }}> ⍜ </Text>
-              </TouchableOpacity>
-            </View>
-          </Camera>
-          {this.state.uploadStatus
-            ? <Text style={{ fontSize: 20,marginBottom: 15 }}>{this.state.uploadStatus}</Text>
-            : <Text></Text>
-          } 
-        </View>
-      );
     }
+    if (hasCameraPermission === false) {
+      return <Text>No access to camera</Text>;
+    }
+    return (
+      <View style={{ flex: 1 }}>
+        <Camera
+          style={{ flex: 1 }}
+          type={this.state.type}
+          ref={ref => {
+            this.camera = ref;
+          }}
+          base64>
+          <View
+            style={{
+              flex: 1,
+              backgroundColor: 'transparent',
+              flexDirection: 'row'
+            }}>
+            <TouchableOpacity
+              style={{
+                flex: 0.1,
+                alignSelf: 'flex-end',
+                alignItems: 'center'
+              }}
+              onPress={() => {
+                this.setState({
+                  type:
+                    // TODO @JohnathanZhou use callback when referencing previous state
+                    this.state.type === Camera.Constants.Type.back
+                      ? Camera.Constants.Type.front
+                      : Camera.Constants.Type.back
+                });
+              }}>
+              <Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}>
+                {' '}
+                Flip{' '}
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={{
+                flex: 0.8,
+                alignSelf: 'flex-end',
+                alignItems: 'center'
+              }}
+              onPress={() => this.snap()}>
+              <Text style={{ fontSize: 40, marginBottom: 10, color: 'white' }}>
+                {' '}
+                ⍜{' '}
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </Camera>
+        {this.state.uploadStatus ? (
+          <Text style={{ fontSize: 20, marginBottom: 15 }}>
+            {this.state.uploadStatus}
+          </Text>
+        ) : (
+          <Text />
+        )}
+      </View>
+    );
   }
 }
