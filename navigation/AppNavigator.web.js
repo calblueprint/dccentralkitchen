acc00a511e3911815f8b3b74ea8b805cdaa49b59
annie.ro98 <anniero@berkeley.edu>
2020-02-07 17:50:27 -0800

    Removing AppNavigator.web.js as it doesn't seem necessary

--- navigation/AppNavigator.web.js
+++
@@ -1,8 +0,0 @@
-import { createBrowserApp } from '@react-navigation/web';
-import { createSwitchNavigator } from 'react-navigation';
-
-const switchNavigator = createSwitchNavigator({
-  // You could add another route here for authentication.
-  // Read more at https://reactnavigation.org/docs/en/auth-flow.html
-});
-export default createBrowserApp(switchNavigator, { history: 'hash' });
