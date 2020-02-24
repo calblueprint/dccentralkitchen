cf6e410031a12b24fd6a0de0b4eb47868328c264
anniero98 <anniero@berkeley.edu>
2020-02-23 16:59:43 -0800

    [0] Development lifecycle improvements  (#23)
    * Add README (draft)
    * Fix up dependencies
    Update ESLint config
    Add Airtable generator and generated files (v1)
    
    * Set up pre-commit hooks with husky and lint-staged
    
    * Disable linting on pre-commit hook for now
    
    * Update ESLint rules for Airtable package + PR template
    
    * Update env management
    
    * Upgrade airtable-schema-generator version
    
    Co-authored-by: Justin Mi  <JustinMi@users.noreply.github.com>

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,27 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    env: {
+      production: {
+        plugins: [
+          [
+            'inline-dotenv',
+            {
+              path: '.env.production'
+            }
+          ]
+        ]
+      },
+      development: {
+        plugins: [
+          [
+            'inline-dotenv',
+            {
+              path: '.env.development'
+            }
+          ]
+        ]
+      }
+    }
   };
 };
