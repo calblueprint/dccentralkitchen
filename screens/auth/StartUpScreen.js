bbeb218de7a33e98c01c4784e3cc789e624a1255
Tommy Poa <tommypoa@berkeley.edu>
2020-03-05 11:54:43 -0800

    Delete web app navigator, change startup screen import path.

--- screens/auth/StartUpScreen.js
+++ screens/auth/StartUpScreen.js
@@ -4,7 +4,7 @@ import * as Permissions from 'expo-permissions';
 import React from 'react';
 import { AsyncStorage, Button, Text, TextInput, View } from 'react-native';
 
-import { ErrorMsg, Input, StartUpContainer } from '../../styles/auth';
+import { ErrorMsg, Input, StartUpContainer } from '../../styled/auth';
 import { BigTitle } from '../../components/BaseComponents';
 
 export default class Login extends React.Component {
