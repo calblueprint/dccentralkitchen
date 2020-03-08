817f56c3c02cdd4697536dff9ba8c1c3d58b12ba
Tommy Poa <tommypoa@berkeley.edu>
2020-03-08 15:25:48 -0700

    Convert horizontal scroll padding from percentage to pixels.

--- styled/product.js
+++ styled/product.js
@@ -30,7 +30,8 @@ export const ProductCardContainer = styled.View`
 
 export const ProductInfoContainer = styled.View`
   width: 80%;
-  padding: 5%;
+  margin-top: 80px;
+  margin-left: 16px;
 `;
 
 export const ProductInfoCaptionContainer = styled.View`
@@ -40,7 +41,7 @@ export const ProductInfoCaptionContainer = styled.View`
 `;
 
 export const ProductInfoImageContainer = styled.View`
-  margin-top: 7.5%;
+  margin-top: 80px;
 `;
 
 export const ProductNoticeContainer = styled.View`
@@ -50,7 +51,9 @@ export const ProductNoticeContainer = styled.View`
 // TODO @tommypoa re-work styles used in ProductDetailedScreen
 export const styles = StyleSheet.create({
   horizontalScroll: {
-    padding: '1%',
+    paddingTop: 5,
+    paddingLeft: 9,
+    paddingBottom: 13,
     alignItems: 'center'
   }
 });
