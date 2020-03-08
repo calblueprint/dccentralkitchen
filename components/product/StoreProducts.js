37bb0d978241381ad892998e2d52bcafff516f51
Tommy Poa <tommypoa@berkeley.edu>
2020-03-08 14:58:15 -0700

    Convert StoreProducts component into a function as with all other components.

--- components/product/StoreProducts.js
+++ components/product/StoreProducts.js
@@ -12,47 +12,44 @@ import ProductCard from './ProductCard';
 import { ProductCardContainer, styles } from '../../styled/product';
 import { SpaceBetweenRowContainer } from '../../styled/shared';
 
-class StoreProducts extends React.Component {
+function StoreProducts({ navigation, store, products }) {
   // TODO @tommypoa or @anniero98 - move this into shared utils with StoreListScreen
-  render() {
-    const { navigation, store, products } = this.props;
-    return (
+  return (
+    <View>
+      <StoreCard store={store} key={store.id} seeProduct={false} />
       <View>
-        <StoreCard store={store} key={store.id} seeProduct={false} />
-        <View>
-          <SpaceBetweenRowContainer>
-            <Title>Products</Title>
-            <ButtonContainer
-              onPress={() =>
-                navigation.navigate('Products', {
-                  products,
-                  navigation,
-                  store
-                })
-              }>
-              <Subhead color="black">See all {products.length}</Subhead>
-            </ButtonContainer>
-          </SpaceBetweenRowContainer>
-          <ScrollView
-            horizontal
-            showsHorizontalScrollIndicator={false}
-            contentContainerStyle={styles.horizontalScroll}>
-            {products.map(product => (
-              // TODO See if there is a better way to pass the props over to a component
-              <ProductCardContainer key={product.id}>
-                <ProductCard
-                  key={product.id}
-                  product={product}
-                  navigation={navigation}
-                  store={store}
-                />
-              </ProductCardContainer>
-            ))}
-          </ScrollView>
-        </View>
+        <SpaceBetweenRowContainer>
+          <Title>Products</Title>
+          <ButtonContainer
+            onPress={() =>
+              navigation.navigate('Products', {
+                products,
+                navigation,
+                store
+              })
+            }>
+            <Subhead color="black">See all {products.length}</Subhead>
+          </ButtonContainer>
+        </SpaceBetweenRowContainer>
+        <ScrollView
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={styles.horizontalScroll}>
+          {products.map(product => (
+            // TODO See if there is a better way to pass the props over to a component
+            <ProductCardContainer key={product.id}>
+              <ProductCard
+                key={product.id}
+                product={product}
+                navigation={navigation}
+                store={store}
+              />
+            </ProductCardContainer>
+          ))}
+        </ScrollView>
       </View>
-    );
-  }
+    </View>
+  );
 }
 
 export default StoreProducts;
