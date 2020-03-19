9d5a66205ddb91f70a7536267713b75a2e6c2192
Kenneth Lien <kennethlien@berkeley.edu>
2020-03-18 18:58:59 -0700

    moved hamburger styling to new file, restyled to match figma

--- components/Hamburger.js
+++ components/Hamburger.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { TouchableOpacity, Button } from 'react-native';
 import { FontAwesome5 } from '@expo/vector-icons';
+import { HamburgerButton } from '../styled/hamburger';
 //TODO: ADD FONTAWESOME ICON
 export default class Hamburger extends React.Component {
   constructor(props) {
@@ -8,28 +9,9 @@ export default class Hamburger extends React.Component {
   }
   render() {
     return (
-      <TouchableOpacity
-        style={{
-          backgroundColor: 'white',
-          width: 50,
-          height: 50,
-          zIndex: 100,
-          position: 'absolute', // comment out this line to see the menu toggle
-          top: 50,
-          left: 15,
-          borderRadius: 23,
-          borderColor: '#ffffff',
-          borderWidth: 4,
-          shadowOpacity: 0.25,
-          shadowRadius: 5,
-          shadowColor: 'black',
-          shadowOffset: { height: 3, width: 4 }
-        }}>
-        <Button
-          color="black"
-          title="="
-          onPress={() => this.props.navigation.toggleDrawer()}></Button>
-      </TouchableOpacity>
+      <HamburgerButton>
+        <FontAwesome5 name="bars" solid size={24} />
+      </HamburgerButton>
     );
   }
 }
---
+++ styled/hamburger.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components/native';
+import { TouchableOpacity } from 'react-native';
+
+//TODO: Fix top to match figma once we figure out iPhone alignment
+export const HamburgerButton = styled(TouchableOpacity)`
+  background-color: #fff;
+  width: 40px;
+  height: 40px;
+  zindex: 100;
+  position: absolute;
+  top: 63px;
+  left: 16px;
+  border-radius: 23px;
+  border-width: 4;
+  border-color: #fff;
+  box-shadow: 0px 3px 4px rgba(0, 0, 0, 0.25);
+  align-items: center;
+  justify-content: center;
+`;
