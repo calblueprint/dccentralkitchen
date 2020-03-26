d1df7e22eac8c233d517798863c7ac8aa9a22f90
Annie Wang <anniedwang02@gmail.com>
2020-03-26 14:52:30 -0700

    Added new nav base components to Products list

--- components/BaseComponents.js
+++ components/BaseComponents.js
@@ -16,8 +16,7 @@ export const NavButton = styled.TouchableOpacity`
   width: 40px;
   height: 40px;
   z-index: 100;
-  position: absolute;
-  top: 40px;
+  top: 0px;
   left: 12px;
   border-radius: 23px;
   align-items: center;
@@ -112,4 +111,26 @@ export const TabSelected = styled.Text`
   font-size: 14px;
   line-height: 20px;
   color: ${props => props.color || Colors.black};
+`;
+
+export const NavHeaderContainer = styled.View`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  padding-top: 60px;
+  padding-bottom: 4px;
+  min-height: 106px;
+  box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.2);
+  margin-bottom: 16px;
+  background-color: ${props =>
+    props.backgroundColor ? props.backgroundColor : Colors.lightest};
+`;
+
+export const NavTitle = styled(Title)`
+  flex: 1;
+  margin-right: 52px;
+  flex-wrap: wrap;
+  text-align: center;
+  color: ${props => (props.color ? props.color : Colors.activeText)};
 `;
--- screens/map/ProductsScreen.js
+++ screens/map/ProductsScreen.js
@@ -2,15 +2,13 @@ import { FontAwesome5 } from '@expo/vector-icons';
 import React from 'react';
 import { View } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
-import Colors from '../../assets/Colors';
-import { Title } from '../../components/BaseComponents';
-import ProductCard from '../../components/product/ProductCard';
 import {
-  BackButton,
-  ProductListContainer,
-  ProductListHeaderContainer
-} from '../../styled/product';
-import { RowContainer } from '../../styled/shared';
+  NavButton,
+  NavHeaderContainer,
+  NavTitle
+} from '../../components/BaseComponents';
+import ProductCard from '../../components/product/ProductCard';
+import { ProductListContainer } from '../../styled/product';
 
 export default class ProductsScreen extends React.Component {
   constructor(props) {
@@ -36,19 +34,12 @@ export default class ProductsScreen extends React.Component {
     const { products, store } = this.props.navigation.state.params;
     return (
       <View>
-        <ProductListHeaderContainer>
-          <RowContainer
-            style={{ width: '100%' }}
-            alignItems="center"
-            justifyContent="center">
-            <BackButton onPress={() => this.props.navigation.goBack(null)}>
-              <FontAwesome5 name="arrow-left" solid size={24} />
-            </BackButton>
-            <Title color={Colors.activeText} style={{ textAlign: 'center' }}>
-              {store.name}
-            </Title>
-          </RowContainer>
-        </ProductListHeaderContainer>
+        <NavHeaderContainer>
+          <NavButton onPress={() => this.props.navigation.goBack()}>
+            <FontAwesome5 name="arrow-left" solid size={24} />
+          </NavButton>
+          <NavTitle>{store.name}</NavTitle>
+        </NavHeaderContainer>
         <ScrollView
           style={{ height: '100%', width: '100%' }}
           showsVerticalScrollIndicator={false}>
