6b28fa8ad1f7fd5f540c5074057c0abc97798422
Annie Wang <anniedwang02@gmail.com>
2020-03-29 23:59:28 -0700

    Fixed rounded corners on See Products button

--- components/store/StoreProductButton.js
+++ components/store/StoreProductButton.js
@@ -13,7 +13,8 @@ function StoreProductButton({ callBack }) {
       width="130px"
       height="35px"
       color={Colors.primaryOrange}
-      onPress={callBack}>
+      onPress={callBack}
+      style={{ borderRadius: 10 }}>
       <ButtonLabel color="#fff">See Products</ButtonLabel>
     </FilledButtonContainer>
   );
