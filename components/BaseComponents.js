30d1d53182f95ad18f522e8cc3d4f1f7223b407e
Annie Wang <anniedwang02@gmail.com>
2020-04-04 11:26:05 -0700

    Added elevation in Android

--- components/BaseComponents.js
+++ components/BaseComponents.js
@@ -139,6 +139,7 @@ export function NavHeaderContainer({
         shadowOffset: { width: 0, height: 3 },
         shadowOpacity: 0.2,
         shadowRadius: 3,
+        elevation: 7,
       }}>
       {children}
     </View>
--- styled/store.js
+++ styled/store.js
@@ -47,6 +47,7 @@ export const SearchBar = styled(ButtonContainer)`
   border-radius: 20px;
   background-color: white;
   box-shadow: 0px 3px 4px rgba(0, 0, 0, 0.25);
+  elevation: 12;
 `;
 
 export const EBTStatusBar = styled.View`
