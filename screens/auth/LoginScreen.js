6f8548fbb8cb5236d547e08f5fe093351ae1fcad
annie.ro98 <anniero@berkeley.edu>
2020-04-08 11:51:12 -0700

    Add back correct version

--- screens/auth/LoginScreen.js
+++ screens/auth/LoginScreen.js
@@ -8,52 +8,46 @@ import AuthTextField from '../../components/AuthTextField';
 import {
   BigTitle,
   ButtonLabel,
+  Caption,
   FilledButtonContainer,
 } from '../../components/BaseComponents';
+import Colors from '../../constants/Colors';
+import { getAllCustomers } from '../../lib/airtable/request';
 import {
-  fieldStateColors,
-  lookupCustomer,
-  signUpFields,
+  formatPhoneNumber,
   updateCustomerPushTokens,
 } from '../../lib/authUtils';
 import {
   AuthScreenContainer,
   BackButton,
-  ErrorMsg,
   FormContainer,
 } from '../../styled/auth';
 import { JustifyCenterContainer } from '../../styled/shared';
 
-export default class Login extends React.Component {
+export default class LogInScreen extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
       phoneNumber: '',
       password: '',
-      errorMsg: '',
+      error: '',
       token: null,
-      indicators: {
-        [signUpFields.PHONENUM]: [fieldStateColors.INACTIVE],
-        [signUpFields.PASSWORD]: [fieldStateColors.INACTIVE],
-      },
     };
   }
 
   componentDidMount() {
     // this.registerForPushNotificationsAsync();
-
     // From SignUpScreen.js, see comment there for details
-    this._notificationSubscription = Notifications.addListener(
-      this._handleNotification
-    );
+    // this._notificationSubscription = Notifications.addListener(
+    //   this._handleNotification
+    // );
   }
 
   // From SignUpScreen. Sign in function. It sets the user token in local storage
   // to be the user ID and then navigates to homescreen.
-  _asyncSignIn = async userId => {
+  _asyncLogIn = async userId => {
     await AsyncStorage.setItem('userId', userId);
-    console.log(userId);
     this.props.navigation.navigate('App');
   };
 
@@ -82,100 +76,87 @@ export default class Login extends React.Component {
 
   // This function will reformat the phone number to (XXX) XXX-XXXX and sign the user in if
   // the user is found.
-  handleSubmit() {
-    let formattedPhoneNumber = this.state.phoneNumber;
-    formattedPhoneNumber = formattedPhoneNumber.replace('[^0-9]', '');
-    formattedPhoneNumber = `(${formattedPhoneNumber.slice(
-      0,
-      3
-    )}) ${formattedPhoneNumber.slice(3, 6)}-${formattedPhoneNumber.slice(
-      6,
-      10
-    )}`;
+  handleSubmit = async () => {
+    const { password, phoneNumber, token } = this.state;
 
-    lookupCustomer(formattedPhoneNumber, this.state.password)
-      .then(customerInfo => {
-        if (customerInfo) {
-          console.log(
-            'Customer lookup successful. Customer Record ID:',
-            customerInfo.custId
-          );
-          this.setState({ errorMsg: '' });
-          return customerInfo;
-        }
-        // If no records exist, resolves with null; set error message
-        this.setState({
-          errorMsg: 'Incorrect phone number or password. Please try again.',
-          phoneNumber: '',
-          password: '',
-        });
-        return null;
-      })
-      .then(customerInfo => {
-        if (customerInfo) {
-          updateCustomerPushTokens(customerInfo, this.state.token)
-            .then(customerId => {
-              if (customerId) {
-                this._asyncSignIn(customerId);
-              }
-              // Otherwise, lookup failed
-              return null;
-            })
-            .catch(err => console.error(err));
-        }
-        // Otherwise, lookup failed
-        return null;
-      })
-      .catch(err => console.error(err));
-  }
-
-  onFocus(signUpField) {
-    const { indicators } = this.state;
-    indicators[signUpField] = fieldStateColors.FOCUSED;
-    this.setState({
-      indicators,
-    });
-  }
-
-  onBlur(signUpField) {
-    const { indicators } = this.state;
-    indicators[signUpField] = fieldStateColors.BLURRED;
-    this.setState({
-      indicators,
-    });
-  }
+    const formattedPhoneNumber = formatPhoneNumber(phoneNumber);
+    try {
+      let error = '';
+      const customers = await getAllCustomers(
+        `AND({Phone Number} = '${formattedPhoneNumber}', {Password} = '${password}')`
+      );
+      // Returns empty array if no customer is found
+      if (customers.length === 1) {
+        const customer = customers[0];
+        // If customer exists, we should update their push tokens
+        await updateCustomerPushTokens(customer, token);
+        // Log in
+        await this._asyncLogIn(customer.id);
+      } else if (customers.length > 1) {
+        // In case of database malformation, may return more than one record
+        // TODO this message is a design edge case
+        error =
+          'Database error: more than one customer found with this login information. Please report an issue so we can fix it for you!';
+      } else {
+        // No customer found
+        error = 'Phone number or password is incorrect.';
+      }
+      this.setState({
+        error,
+      });
+    } catch (err) {
+      console.error('[LogInScreen] Airtable:', err);
+    }
+  };
 
   render() {
+    const { phoneNumber, password } = this.state;
+    const logInPermission =
+      phoneNumber.length === 10 &&
+      password.length >= 8 &&
+      this.state.error === '';
+
     return (
       <AuthScreenContainer>
         <BackButton onPress={() => this.props.navigation.goBack(null)}>
           <FontAwesome5 name="arrow-left" solid size={24} />
         </BackButton>
-        <BigTitle>Log in</BigTitle>
+        <BigTitle>Log In</BigTitle>
         <FormContainer>
           <AuthTextField
             fieldType="Phone Number"
-            color={this.state.indicators[signUpFields.PHONENUM]}
             value={this.state.phoneNumber}
-            onBlurCallback={() => this.onBlur(signUpFields.PHONENUM)}
-            onFocusCallback={() => this.onFocus(signUpFields.PHONENUM)}
-            changeTextCallback={text => this.setState({ phoneNumber: text })}
+            changeTextCallback={async text => {
+              this.setState({ phoneNumber: text, error: '' });
+            }}
+            // Display error indicator ('no text') only when login fails
+            error={this.state.error ? ' ' : ''}
           />
           <AuthTextField
             fieldType="Password"
-            color={this.state.indicators[signUpFields.PASSWORD]}
             value={this.state.password}
-            onBlurCallback={() => this.onBlur(signUpFields.PASSWORD)}
-            onFocusCallback={() => this.onFocus(signUpFields.PASSWORD)}
-            changeTextCallback={text => this.setState({ password: text })}
+            changeTextCallback={async text => {
+              this.setState({ password: text, error: '' });
+            }}
+            // Display error indicator ('no text') only when login fails
+            error={this.state.error ? ' ' : ''}
           />
+          <Caption
+            style={{ alignSelf: 'center', fontSize: 14 }}
+            color={Colors.error}>
+            {this.state.error}
+          </Caption>
         </FormContainer>
         <JustifyCenterContainer>
           <FilledButtonContainer
-            style={{ alignSelf: 'flex-end' }}
+            style={{ marginTop: 104 }}
+            color={
+              !logInPermission ? Colors.lightestGreen : Colors.primaryGreen
+            }
             width="100%"
-            onPress={() => this.handleSubmit()}>
-            <ButtonLabel color="#fff">Log in</ButtonLabel>
+            onPress={() => this.handleSubmit()}
+            disabled={!logInPermission}>
+            <ButtonLabel color={Colors.lightest}>Log in</ButtonLabel>
           </FilledButtonContainer>
 
           {/* TODO @tommypoa: Forgot password functionality
@@ -186,7 +167,6 @@ export default class Login extends React.Component {
             </ButtonContainer>
           </ForgotPasswordButtonContainer> */}
         </JustifyCenterContainer>
-        <ErrorMsg>{this.state.errorMsg}</ErrorMsg>
       </AuthScreenContainer>
     );
   }
