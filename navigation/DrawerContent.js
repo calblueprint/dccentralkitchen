73848f89bf1f03f8147a1a7c6f171f4da1b15d21
Annie Wang <anniedwang02@gmail.com>
2020-04-12 12:12:30 -0700

    Added setUser in DrawerContent

--- navigation/DrawerContent.js
+++ navigation/DrawerContent.js
@@ -1,6 +1,7 @@
 import { DrawerItemList } from '@react-navigation/drawer';
 import React from 'react';
 import { AsyncStorage, Linking, TouchableOpacity, View } from 'react-native';
+import * as Sentry from 'sentry-expo';
 import { Title } from '../components/BaseComponents';
 import Colors from '../constants/Colors';
 import { getCustomersById } from '../lib/airtable/request';
@@ -24,6 +25,14 @@ class DrawerContent extends React.Component {
       } else {
         customer = { name: 'Guest' };
       }
+      Sentry.configureScope(scope => {
+        scope.setExtra('setUserScreen', 'DrawerContent');
+        scope.setUser({
+          id: customerId,
+          username: customer.name,
+          phoneNumber: customer.phoneNumber,
+        });
+      });
       this.setState({ customer, isLoading: false });
     } catch (err) {
       console.error('[DrawerContent] Airtable:', err);
