30e2968fe1b70f6df4898d39b33a3aef49d608b5
Thu Nguyen <tmnguyen@berkeley.edu>
2020-04-13 00:21:59 -0700

    drawer content

--- navigation/DrawerContent.js
+++ navigation/DrawerContent.js
@@ -1,79 +1,92 @@
+import { DrawerItemList } from '@react-navigation/drawer';
+import { useFocusEffect, useNavigation } from '@react-navigation/native';
 import React from 'react';
 import { AsyncStorage, Linking, TouchableOpacity, View } from 'react-native';
-import { DrawerItems } from 'react-navigation-drawer';
 import { Title } from '../components/BaseComponents';
 import Colors from '../constants/Colors';
 import { getCustomersById } from '../lib/airtable/request';
 
-class DrawerContent extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      customer: null,
-      link: 'http://tiny.cc/RewardsFeedback',
-      isLoading: true,
-    };
+function DrawerContent(props) {
+  const [customer, setCustomer] = React.useState(null);
+  const [link, _] = React.useState('http://tiny.cc/RewardsFeedback');
+  const [isLoading, setIsLoading] = React.useState(true);
+  const navigation = useNavigation();
+
+  useFocusEffect(
+    React.useCallback(() => {
+      let isActive = true;
+
+      const fetchUser = async () => {
+        try {
+          const customerId = await AsyncStorage.getItem('userId');
+          let cust = null;
+          if (customerId != null) {
+            cust = await getCustomersById(customerId);
+          } else {
+            cust = { name: 'Guest' };
+          }
+          if (isActive) {
+            setCustomer(cust);
+            setIsLoading(false);
+          }
+        } catch (err) {
+          console.error('[DrawerContent] Airtable:', err);
+        }
+      };
+
+      fetchUser();
+
+      return () => {
+        isActive = false;
+      };
+    }, [])
+  );
+
+  if (isLoading) {
+    return null;
   }
 
-  async componentDidMount() {
-    try {
-      const customerId = await AsyncStorage.getItem('userId');
-      const customer = await getCustomersById(customerId);
-      this.setState({ customer, isLoading: false });
-    } catch (err) {
-      console.error('[DrawerContent] Airtable:', err);
-    }
-  }
-
-  _logout = async () => {
-    AsyncStorage.clear();
-    this.props.navigation.navigate('Auth');
-  };
-
-  render() {
-    if (this.state.isLoading) {
-      return null;
-    }
-    return (
+  return (
+    <View
+      style={{
+        display: 'flex',
+        flex: 1,
+        flexDirection: 'column',
+      }}>
       <View
         style={{
+          backgroundColor: Colors.black,
+          height: 114,
           display: 'flex',
+          flexDirection: 'row',
+          alignItems: 'flex-end',
+          padding: 16,
+        }}>
+        <Title style={{ color: 'white' }}>{customer.name}</Title>
+      </View>
+      <DrawerItemList {...props} />
+      <View
+        style={{
           flex: 1,
           flexDirection: 'column',
+          justifyContent: 'flex-end',
+          verticalAlign: 'bottom',
         }}>
-        <View
-          style={{
-            backgroundColor: Colors.black,
-            height: 114,
-            display: 'flex',
-            flexDirection: 'row',
-            alignItems: 'flex-end',
-            padding: 16,
+        <TouchableOpacity
+          style={{ padding: 16 }}
+          onPress={() => Linking.openURL(link)}>
+          <Title>Report Issue</Title>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={{ paddingLeft: 16, paddingBottom: 21 }}
+          onPress={() => {
+            AsyncStorage.clear().then(navigation.navigate('Auth'));
           }}>
-          <Title style={{ color: 'white' }}>{this.state.customer.name}</Title>
-        </View>
-        <DrawerItems {...this.props} />
-        <View
-          style={{
-            flex: 1,
-            flexDirection: 'column',
-            justifyContent: 'flex-end',
-            verticalAlign: 'bottom',
-          }}>
-          <TouchableOpacity
-            style={{ padding: 16 }}
-            onPress={() => Linking.openURL(this.state.link)}>
-            <Title>Report Issue</Title>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={{ paddingLeft: 16, paddingBottom: 21 }}
-            onPress={() => this._logout()}>
-            <Title>Log Out</Title>
-          </TouchableOpacity>
-        </View>
+          <Title>Log Out</Title>
+        </TouchableOpacity>
       </View>
-    );
-  }
+    </View>
+  );
 }
 
 export default DrawerContent;
