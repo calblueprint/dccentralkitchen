a8ee3cc33138059b3902fb3d79baed9faa69dab3
Thu Nguyen <tmnguyen@berkeley.edu>
2020-04-13 13:22:56 -0700

    close drawer on logout

--- navigation/DrawerContent.js
+++ navigation/DrawerContent.js
@@ -56,7 +56,12 @@ function DrawerContent(props) {
   const logout = async () => {
     AsyncStorage.clear();
     Sentry.configureScope(scope => scope.clear());
-    navigation.navigate('Auth');
+    // navigation.navigate('DrawerClose');
+    // navigation.navigate('Auth');
+    setTimeout(function() {
+      navigation.navigate('Auth');
+    }, 500);
+    props.navigation.closeDrawer();
   };
 
   return (
