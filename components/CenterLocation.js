34dcc4d576a7172c61dac73778e98aa10ee96e11
Tommy Poa <35573990+tommypoa@users.noreply.github.com>
2020-04-13 13:28:30 +0800

    Map Marker Icons (#87)
    * Implement store icon markers. Store animation bug not resolved.
    
    * Change image name, fix animate bug.
    
    * Visual changes for focused and resting markers.
    
    * Replace images with higher resolution ones.
    
    * Rename files for capitalization.
    
    * Updated Center Location button styling

--- components/CenterLocation.js
+++ components/CenterLocation.js
@@ -1,13 +1,21 @@
 import { FontAwesome5 } from '@expo/vector-icons';
 import PropTypes from 'prop-types';
 import React from 'react';
+import Colors from '../constants/Colors';
 import { HamburgerButton } from '../styled/hamburger';
 
 function CenterLocation(props) {
   const { callBack } = props.callBack;
   return (
-    <HamburgerButton onPress={() => callBack()} style={{ marginRight: 24 }}>
-      <FontAwesome5 name="location-arrow" solid size={20} color="blue" />
+    <HamburgerButton
+      onPress={() => callBack()}
+      style={{ marginRight: 24, marginTop: 2 }}>
+      <FontAwesome5
+        name="location-arrow"
+        solid
+        size={20}
+        color={Colors.primaryGreen}
+      />
     </HamburgerButton>
   );
 }
