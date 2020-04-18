036b19188b4bf15ca9e5b836bef3aadc9c591776
Tommy Poa <35573990+tommypoa@users.noreply.github.com>
2020-04-19 03:07:32 +0800

    Additional reward cards for point history (#93)
    * Implement additional reward cards for point history.
    
    * PR change requests: change reward card order, cleanup.

--- components/rewards/Transaction.js
+++ components/rewards/Transaction.js
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import { View } from 'react-native';
 import Colors from '../../constants/Colors';
 import { displayDollarValue } from '../../lib/common';
 import { ContentContainer, TransactionCard } from '../../styled/transaction';
@@ -11,7 +12,14 @@ import CircleIcon from '../CircleIcon';
  * */
 
 function Transaction(props) {
-  const { date, storeName, pointsEarned, totalSale } = props;
+  const {
+    date,
+    storeName,
+    pointsEarned,
+    totalSale,
+    rewardsUnlocked,
+    rewardsApplied,
+  } = props;
   const options = {
     weekday: 'short',
     year: 'numeric',
@@ -19,22 +27,62 @@ function Transaction(props) {
     day: 'numeric',
   };
   return (
-    <TransactionCard>
-      <CircleIcon
-        icon="check"
-        iconColor={Colors.primaryGreen}
-        circleColor={Colors.lightestGreen}
-      />
-      <ContentContainer>
-        <Caption color={Colors.secondaryText}>
-          {`${date.toLocaleDateString('en-US', options)} • ${storeName}`}
-        </Caption>
-        <Subhead>{`${pointsEarned} points earned`}</Subhead>
-        <Caption color={Colors.secondaryText}>
-          {`for ${displayDollarValue(totalSale || 0)} of healthy products`}
-        </Caption>
-      </ContentContainer>
-    </TransactionCard>
+    <View>
+      {/* Display rewards unlocked */}
+      {[...Array(rewardsUnlocked).keys()].map(() => (
+        <TransactionCard>
+          <CircleIcon
+            icon="star"
+            iconColor={Colors.primaryGreen}
+            circleColor={Colors.lightestGreen}
+          />
+          <ContentContainer>
+            <Caption color={Colors.secondaryText}>
+              {`${date.toLocaleDateString('en-US', options)} • ${storeName}`}
+            </Caption>
+            <Subhead>{`$5 reward unlocked`}</Subhead>
+            <Caption color={Colors.secondaryText}>
+              {`for 500 earned points`}
+            </Caption>
+          </ContentContainer>
+        </TransactionCard>
+      ))}
+
+      {/* Display points earned */}
+      <TransactionCard>
+        <CircleIcon
+          icon="check"
+          iconColor={Colors.primaryGreen}
+          circleColor={Colors.lightestGreen}
+        />
+        <ContentContainer>
+          <Caption color={Colors.secondaryText}>
+            {`${date.toLocaleDateString('en-US', options)} • ${storeName}`}
+          </Caption>
+          <Subhead>{`${pointsEarned} points earned`}</Subhead>
+          <Caption color={Colors.secondaryText}>
+            {`for ${displayDollarValue(totalSale || 0)} of healthy products`}
+          </Caption>
+        </ContentContainer>
+      </TransactionCard>
+
+      {/* Display rewards applied */}
+      {[...Array(rewardsApplied).keys()].map(() => (
+        <TransactionCard>
+          <CircleIcon
+            icon="star"
+            iconColor={Colors.primaryOrange}
+            circleColor={Colors.lightestOrange}
+          />
+          <ContentContainer>
+            <Caption color={Colors.secondaryText}>
+              {`${date.toLocaleDateString('en-US', options)} • ${storeName}`}
+            </Caption>
+            <Subhead>{`$5 reward redeemed`}</Subhead>
+          </ContentContainer>
+        </TransactionCard>
+      ))}
+    </View>
   );
 }
 
@@ -43,12 +91,16 @@ Transaction.propTypes = {
   storeName: PropTypes.array,
   pointsEarned: PropTypes.number,
   totalSale: PropTypes.number,
+  rewardsUnlocked: PropTypes.number,
+  rewardsApplied: PropTypes.number,
 };
 
 Transaction.defaultProps = {
   storeName: null,
   pointsEarned: 0,
   totalSale: 0,
+  rewardsUnlocked: null,
+  rewardsApplied: 0,
 };
 
 export default Transaction;
