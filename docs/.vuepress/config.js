e27c26850317327a95f7a77a35c26c12b97fd68b
annie.ro98 <anniero@berkeley.edu>
2020-04-26 22:55:02 -0700

    Initial layout (guide, dev subfolders) for VuePress documentation. Install VuePress and Surge

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,50 @@
+module.exports = {
+  title: 'Healthy Corners Rewards',
+  description:
+    'Documentation for the projects developed by @calblueprint for DC Central Kitchen',
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Admin Guide', link: '/guide/' },
+      { text: 'Developer Docs', link: '/dev/' },
+      {
+        text: 'Github',
+        items: [
+          {
+            text: 'Customer App',
+            link: 'https://github.com/calblueprint/dccentralkitchen',
+          },
+          {
+            text: 'Clerk App',
+            link: 'https://github.com/calblueprint/dccentralkitchen-clerks',
+          },
+          {
+            text: 'Backend',
+            link: 'https://github.com/calblueprint/dccentralkitchen-node',
+          },
+        ],
+      },
+      {
+        text: 'Airtable',
+        items: [
+          {
+            text: 'Development',
+            link:
+              'https://airtable.com/invite/l?inviteId=invesIqGWvEme1unA&inviteToken=d2b75868ccb369533de5ecc1856f5b51ce59a1f8634a5e63f269b49ae6c7c829',
+          },
+          {
+            text: 'Production',
+            link:
+              'https://airtable.com/invite/l?inviteId=invs00iNRdVREHGdd&inviteToken=c60062f2439511603a97ca005e1a03ee9226d9bab0149ff469034475e08381e6',
+          },
+          {
+            text: 'Workspace',
+            link:
+              'https://airtable.com/invite/l?inviteId=invFPVCCPnoHZ0htX&inviteToken=8c4d57af33a94c6b1527d75df1f13c7ecb9a98b38ae3eb432700445e1fb2bc6d',
+          },
+        ],
+      },
+    ],
+    sidebar: ['/', '/guide/', '/dev/'],
+  },
+};
---
+++ docs/README.md
@@ -0,0 +1,5 @@
+## Home
+
+A guide to the customer and companion clerk applications developed with Expo, React Native, and Airtable and built for DC Central Kitchen's "Healthy Corners" program by @calblueprint's 2019-2020 team.
+
+Run `yarn docs:dev` to get the documentation site running locally (it'll default to localhost:8080, but terminal output will tell you)
