8acaf3672df3af69caa96b10dbd7bb749879829c
Tommy Poa <35573990+tommypoa@users.noreply.github.com>
2020-04-30 13:58:41 +0800

    Mid-fi Customer Onboarding (#94)
    * Implement mid-fi onboarding.
    
    * Fix loading rendering issue with carousel.
    
    * PR review changes - responsive onboarding screen width, dotstyle style sheet.
    
    * Made icons. Space between not working.
    
    * Screens responsive-ish.
    
    * Deleted unused images

---
+++ constants/Onboarding.js
@@ -0,0 +1,31 @@
+export const ONBOARDING_CONTENT = [
+  {
+    title: 'Welcome to Healthy Corners!',
+    body: 'Start finding fresh and healthy food at discount prices',
+    icons: [],
+  },
+  {
+    title: 'Find stores for you',
+    body:
+      'Explore the map to discover nearby stores stocking healthy fruits and vegetables',
+    icons: ['search', 'map-marker-alt', 'apple-alt'],
+  },
+  {
+    title: 'Know what’s in stock',
+    body:
+      'See what products are available in stores before you even leave  the house',
+    icons: ['home', 'shopping-basket', 'pepper-hot'],
+  },
+  {
+    title: 'Shop healthy and save',
+    body:
+      'Sign up for Healthy Rewards to earn $5 rewards when you buy healthy products',
+    icons: ['star', 'money-bill', 'carrot'],
+  },
+  {
+    title: 'Stay informed',
+    body:
+      'Access our resource database to help you continue eating and living healthy',
+    icons: ['heart', 'balance-scale', 'user'],
+  },
+];
