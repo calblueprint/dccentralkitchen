e3386f33ce9650b20c282c29dd97b82756a65e90
Annie Wang <anniedwang02@gmail.com>
2020-05-18 00:28:53 -0400

    Remove default Expo assets

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import { Ionicons } from '@expo/vector-icons';
 import { AppLoading } from 'expo';
-import { Asset } from 'expo-asset';
 import * as Font from 'expo-font';
 import React, { useState } from 'react';
 import { Platform, StatusBar, StyleSheet, View } from 'react-native';
@@ -43,10 +42,6 @@ export default function App(props) {
 
 async function loadResourcesAsync() {
   await Promise.all([
-    Asset.loadAsync([
-      require('./assets/images/robot-dev.png'),
-      require('./assets/images/robot-prod.png'),
-    ]),
     Font.loadAsync({
       // This is the font that we are using for our tab bar
       ...Ionicons.font,
