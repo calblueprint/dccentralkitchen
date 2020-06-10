1aee175091816582b1f0bd0ce62cddff84a084ff
Annie Wang <anniedwang02@gmail.com>
2020-06-10 18:10:16 -0400

    Fix button alignment for Android

--- styled/hamburger.js
+++ styled/hamburger.js
@@ -1,3 +1,4 @@
+import { Platform } from 'react-native';
 import styled from 'styled-components/native';
 import { NavButtonContainer } from '../components/BaseComponents';
 import Colors from '../constants/Colors';
@@ -7,5 +8,5 @@ export const HamburgerButton = styled(NavButtonContainer)`
   background-color: ${Colors.bgLight};
   box-shadow: 0px 3px 4px rgba(0, 0, 0, 0.25);
   elevation: 12;
-  padding-top: 3px;
+  padding-top: ${Platform.OS === 'ios' ? '3px' : '1px'};
 `;
