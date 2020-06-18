82106f028e6cc9b2ec01d809d1e97bbcfb9a31d3
Annie Wang <anniedwang02@gmail.com>
2020-06-17 21:35:54 -0400

    Remove clerk training store ID

--- constants/RecordIds.js
+++ constants/RecordIds.js
@@ -3,7 +3,6 @@ import { env } from '../environment';
 const RecordIds = {
   testCustomerId: null,
   guestCustomerId: null,
-  clerkTrainingStoreId: null,
   defaultStoreId: null,
 };
 
@@ -13,14 +12,12 @@ const RecordIds = {
 if (env === 'dev') {
   RecordIds.testCustomerId = 'recimV9zs2StWB2Mj';
   RecordIds.guestCustomerId = 'recLKK7cZHboMPEB8';
-  RecordIds.clerkTrainingStoreId = 'reck65q6wsvyQvnLf';
   RecordIds.defaultStoreId = 'recKmetaavnMWXVrk';
 
   // IDs from PROD base
 } else if (env === 'prod') {
   RecordIds.testCustomerId = 'recomWMtzSUQCcIvr';
   RecordIds.guestCustomerId = 'recxEGfvExP4Dv8nr';
-  RecordIds.clerkTrainingStoreId = 'recq6630DixVw63un';
   RecordIds.defaultStoreId = 'recQmf64hlp9CyBas';
 }
 
