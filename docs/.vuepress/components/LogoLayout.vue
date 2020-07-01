d05a5bbae6eb48122a99a87b2dd4fb8082aec6ee
Annie Wang <anniedwang02@gmail.com>
2020-07-01 14:24:08 -0400

    FAQ base setup

---
+++ docs/.vuepress/components/LogoLayout.vue
@@ -0,0 +1,76 @@
+<template>
+  <main class="home" aria-labelledby="main-title">
+    <main class="page">
+      <header class="hero">
+        <img
+          v-if="data.heroImage"
+          :src="$withBase(data.heroImage)"
+          :alt="data.heroAlt || 'hero'"
+        />
+      </header>
+    </main>
+    <Layout />
+    <main class="page">
+      <div class="footer" v-if="data.footer">
+        <a :href="data.footerLink || null" target="_blank">
+          {{ data.footer }}
+        </a>
+      </div>
+    </main>
+  </main>
+</template>
+
+<script>
+import Layout from '@vuepress/theme-default/layouts/Layout.vue';
+export default {
+  extends: Layout,
+  components: { Layout },
+  computed: {
+    data() {
+      return this.$page.frontmatter;
+    },
+  },
+};
+</script>
+
+<style lang="stylus">
+.home
+  padding $navbarHeight 2rem 0
+  max-width 960px
+  margin 0px auto
+  display block
+  .hero
+    margin 1em auto 0 auto
+    padding-left 2.5rem
+    max-width 760px
+    margin 3em auto 0 auto
+    img
+      max-width 300px
+      max-height 100px
+      display block
+      margin 0 0
+  .footer
+    padding 2.5rem
+    border-top 0px solid $borderColor
+    text-align center
+    color lighten($textColor, 25%)
+.page
+  padding-bottom 0
+
+@media (max-width: 959px)
+  .home
+    .hero
+      padding 0 2rem
+
+@media (max-width: $MQMobileNarrow)
+  .home
+    padding $navbarHeight 2rem 0
+    .hero
+      margin 1em auto 0 auto
+      padding 0 1.5rem
+      img
+        max-width 200px
+        max-height 70px
+  .page
+    padding-bottom 0
+</style>
---
+++ docs/faq.md
@@ -0,0 +1,38 @@
+---
+editLink: false
+sidebar: auto
+heroImage: /hc_splash.png
+layout: LogoLayout
+footer: Click here to use our online contact form
+footerLink: https://tiny.cc/RewardsFeedback
+---
+# Frequently Asked Questions
+
+## Healthy Corners
+
+## SNAP Matching and Coupons
+
+## Healthy Rewards
+
+<br/>
+
+::: faq How do I join the Healthy Rewards program?
+You can create a Healthy Rewards account by downloading the Healthy Corners app or by registering in person at a participating Healthy Corners store.
+
+**Healthy Corners App**
+1. Download the Healthy Corners app from the app store
+2. On the opening screen, select Get Started and Sign Up 
+3. Enter your name, phone number, and set a password. 
+4. Enter the code sent to you by text message to verify your phone number.
+5. Once your account is set up, you can start earning points and using Healthy Rewards!
+
+**In person**
+1. At a participating Healthy Corners store, tell the clerk you want to sign up for Healthy Rewards
+2. They will ask for your name and phone number to register your account. You can then start earning points and using Healthy Rewards!
+3. (Optional) If you registered for an account in person and want to use the Healthy Corners app to see your points and rewards progress:
+   1. Download the Healthy Corners app from the app store
+   2. On the opening screen, select Get Started and Set a password
+   3. Enter the phone number you used to sign up in store 
+::: 
+
+## Account
