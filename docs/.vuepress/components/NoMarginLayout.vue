370b17e15bea72db8cbf1f47e95a22f842985eb6
Annie Wang <anniedwang02@gmail.com>
2020-07-18 11:34:43 -0400

    Finalize margin layout for mini-faq

--- docs/.vuepress/components/NoMarginLayout.vue
+++ docs/.vuepress/components/NoMarginLayout.vue
@@ -1,5 +1,5 @@
 <template>
-  <Layout />
+  <Content class="theme-default-content" />
 </template>
 
 <script>
@@ -7,17 +7,20 @@ import Layout from '@vuepress/theme-default/layouts/Layout.vue';
 export default {
   extends: Layout,
   components: { Layout },
+  computed: {
+    data() {
+      return this.$page.frontmatter;
+    },
+  },
 };
 </script>
 
-@import url('https://fonts.googleapis.com/css2?family=Poppins&display=swap');
-
 <style lang="stylus">
 body
-  font-family "Poppins"
+  font-family 'Poppins'
 @media (max-width: $MQMobileNarrow)
   .theme-default-content:not(.custom)
-    padding 0
+    padding 1px 0 0 0
     *:first-child
       margin-top 1px
 </style>
