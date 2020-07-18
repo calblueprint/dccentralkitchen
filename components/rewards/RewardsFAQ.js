bb4e45b5324db5a4436f8d57250947500a659590
Annie Wang <anniedwang02@gmail.com>
2020-07-18 14:09:20 -0400

    Add mini FAQ as webview

--- components/rewards/RewardsFAQ.js
+++ components/rewards/RewardsFAQ.js
@@ -1,8 +1,8 @@
 import * as WebBrowser from 'expo-web-browser';
 import React from 'react';
-import { ScrollView, View } from 'react-native';
-import { Question } from '../../styled/rewards';
-import { Body, ButtonLabel, Title } from '../BaseComponents';
+import { Dimensions, ScrollView, View } from 'react-native';
+import AutoHeightWebView from 'react-native-autoheight-webview';
+import { ButtonLabel, Title } from '../BaseComponents';
 
 /**
  * @prop
@@ -18,30 +18,16 @@ function RewardsFAQ() {
         marginTop: 24,
         marginBottom: 100,
       }}>
-      <View style={{ display: 'flex', maxHeight: 1750 }}>
-        <Title style={{}}>Healthy Rewards FAQs</Title>
-        <Question>
-          Who can use Healthy Rewards? Do I have to receive SNAP/EBT to qualify?
-        </Question>
-        <Body>
-          Anyone can use Healthy Rewards! You do not have to use SNAP/EBT to
-          qualify. Just sign up for an account in the Healthy Corners app to
-          start earning points at participating stores.
-        </Body>
-        <Question>Do I have to pay a membership fee?</Question>
-        <Body>
-          The Healthy Rewards program is free. Just use your registered phone
-          number at checkout in a participating Healthy Corners store to earn
-          points.
-        </Body>
-        <Question>
-          What is the difference between Healthy Rewards and SNAP matching?
-        </Question>
-        <Body>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris
-        </Body>
+      <View style={{ display: 'flex' }}>
+        <Title style={{ marginBottom: 16 }}>Frequently Asked Questions</Title>
+        <AutoHeightWebView
+          style={{ width: Dimensions.get('window').width - 32 }}
+          source={{
+            uri: 'http://localhost:8080/faq-mini.html',
+          }}
+          scrollEnabled={false}
+          showsVerticalScrollIndicator={false}
+        />
         <ButtonLabel
           textAlign="left"
           noCaps
@@ -52,7 +38,7 @@ function RewardsFAQ() {
               'https://healthycorners.calblueprint.org/faq.html'
             )
           }>
-          More Healthy Rewards FAQs
+          More Healthy Corners FAQs
         </ButtonLabel>
       </View>
     </ScrollView>
