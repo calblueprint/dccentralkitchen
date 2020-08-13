75704c37431c748a15bbe88edbcde6fb64a3703a
Annie Wang <anniedwang02@gmail.com>
2020-08-12 22:54:40 -0400

    Remove old files

--- navigation/stack_navigators/AuthStack.js
+++ navigation/stack_navigators/AuthStack.js
@@ -2,13 +2,9 @@ import { createStackNavigator } from '@react-navigation/stack';
 import React from 'react';
 import Colors from '../../constants/Colors';
 import CompleteSignUpScreen from '../../screens/auth/CompleteSignUpScreen';
-import LogInScreen from '../../screens/auth/LogInScreen';
 import OnboardingScreen from '../../screens/auth/OnboardingScreen';
-import PasswordResetScreen from '../../screens/auth/PasswordResetScreen';
 import PhoneNumberScreen from '../../screens/auth/PhoneNumberScreen';
-import SignUpScreen from '../../screens/auth/SignUpScreen';
 import VerificationScreen from '../../screens/auth/VerificationScreen';
-import WelcomeScreen from '../../screens/auth/WelcomeScreen';
 
 const AuthStack = createStackNavigator();
 
@@ -20,16 +16,12 @@ export default function AuthStackNavigator() {
         cardStyle: { backgroundColor: Colors.bgLight },
       }}>
       <AuthStack.Screen name="Onboarding" component={OnboardingScreen} />
-      <AuthStack.Screen name="Welcome" component={WelcomeScreen} />
       <AuthStack.Screen name="PhoneNumber" component={PhoneNumberScreen} />
       <AuthStack.Screen name="Verify" component={VerificationScreen} />
       <AuthStack.Screen
         name="CompleteSignUp"
         component={CompleteSignUpScreen}
       />
-      <AuthStack.Screen name="SignUp" component={SignUpScreen} />
-      <AuthStack.Screen name="LogIn" component={LogInScreen} />
-      <AuthStack.Screen name="Reset" component={PasswordResetScreen} />
     </AuthStack.Navigator>
   );
 }
